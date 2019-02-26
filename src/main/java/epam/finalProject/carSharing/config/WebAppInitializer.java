45292e01e2a661d9dec5f369752e7ddd6fba287f
MWladislav <matujzo_vv_16@mf.grsu.by>
2019-02-26 20:03:42 +0300

    Default Changelist

---
+++ src/main/java/epam/finalProject/carSharing/config/WebAppInitializer.java
@@ -0,0 +1,28 @@
+package epam.finalProject.carSharing.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext container) {
+        AnnotationConfigWebApplicationContext context
+                = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation("epam.finalProject.carSharing.config");
+
+        container.addFilter("springSecurityFilterChain", new DelegatingFilterProxy());
+        container.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic dispatcher = container
+                .addServlet("dispatcher", new DispatcherServlet(context));
+
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
